8ba43a9cf297a5c80cb918ffa038b00b2b25df12
Hercules <hyukkyulee@alumni.emory.edu>
2021-08-09 12:44:37 +0900

    img radio button solved
    using label tag at the foremost outside and removing '/' from input and img tag was the key

--- popup.js
+++ popup.js
@@ -9,13 +9,13 @@
     const MIN_RELOAD_INTERVAL_MILLISECONDS = 2000;
 
     const init = () => {
-        document.getElementById('bot-token').value = localStorage.getItem('NAVER_VACCINE_MACRO::bot-token');
-        document.getElementById('chat-id').value = localStorage.getItem('NAVER_VACCINE_MACRO::chat-id');
+        // document.getElementById('bot-token').value = localStorage.getItem('NAVER_VACCINE_MACRO::bot-token');
+        // document.getElementById('chat-id').value = localStorage.getItem('NAVER_VACCINE_MACRO::chat-id');
         let savedInterval = parseInt(localStorage.getItem('NAVER_VACCINE_MACRO::interval'));
+        
+        let savedInterval_max = parseInt(localStorage.getItem('NAVER_VACCINE_MACRO::interval_max')); // To Do: 주기를 random으로 바꾸기
 
-        // console.log("saved interval : ");
-        // console.log(savedInterval);
-        // console.log(Number.isInteger(savedInterval));
+
 
         if (!savedInterval || savedInterval < MIN_RELOAD_INTERVAL_MILLISECONDS)
         {
@@ -24,48 +24,52 @@
             localStorage.setItem('NAVER_VACCINE_MACRO::interval', savedInterval);
         }
         document.getElementById('interval').value = savedInterval;
-        document.getElementById('reserve_test').checked = localStorage.getItem('NAVER_VACCINE_MACRO::reserve_test') == 1;
+        // document.getElementById('reserve_test').checked = localStorage.getItem('NAVER_VACCINE_MACRO::reserve_test') == 1;
 
-        const graphqlResult = document.getElementById('graphql_result');
-        graphqlResult.value = localStorage.getItem('NAVER_VACCINE_MACRO::hospital_list');
+        // const graphqlResult = document.getElementById('graphql_result');
+        // graphqlResult.value = localStorage.getItem('NAVER_VACCINE_MACRO::hospital_list');
 
-        const kakaoBody = document.getElementById('left_by_count_coords');
-        kakaoBody.value = localStorage.getItem('NAVER_VACCINE_MACRO::kakao_body');
+        // const kakaoBody = document.getElementById('left_by_count_coords');
+        // kakaoBody.value = localStorage.getItem('NAVER_VACCINE_MACRO::kakao_body');
 
-        if (graphqlResult.value) extract_list();
-
-        let radios = document.querySelectorAll('input[name="select_vaccine"]');
-        for (let i = 0; i < radios.length; i++) {
-            radios[i].addEventListener('click', function () {
+        // if (graphqlResult.value) extract_list();
+        
+        document.querySelectorAll('.vaccines').forEach(item => {
+            item.addEventListener('click', event => {
+                console.log(event.target.id);
+                localStorage.setItem('NAVER_VACCINE_MACRO::vaccine', event.target.id);
                 saveVaccine();
-            });
-        }
+            })
+          })
+        
 
         let intervalText = document.getElementById('interval');
         intervalText.addEventListener('change', function (e) {
-            saveVaccine();
+            console.log('interval changed');
+            saveVaccine(); method에서 interval 업데이트 됨
+            
         });
 
-        let testCheck = document.getElementById('reserve_test');
-        testCheck.addEventListener('click', function (e) {
-            saveVaccine();
-        });
+        // let testCheck = document.getElementById('reserve_test');
+        // testCheck.addEventListener('click', function (e) {
+        //     saveVaccine();
+        // });
 
-        let selectedId = localStorage.getItem('NAVER_VACCINE_MACRO::vaccine');
-        if (selectedId == null) {
-            document.getElementById("target_vaccine_all").checked = true;
-            return;
-        }
-        let target = document.getElementById(selectedId);
-        if (target == null)
-            return;
-        target.checked = true;
+        // let selectedId = localStorage.getItem('NAVER_VACCINE_MACRO::vaccine');
+        // if (selectedId == null) {
+        //     document.getElementById("target_vaccine_all").checked = true;
+        //     return;
+        // }
+        // let target = document.getElementById(selectedId);
+        // if (target == null)
+        //     return;
+        // target.checked = true;
     }
 
     const saveVaccine = () => {
-        let selected = document.querySelector('input[name="select_vaccine"]:checked');
+        // let selected = document.querySelector('input[name="select_vaccine"]:checked');
         let interval = document.getElementById('interval').value;
-        let isReserveTest = document.getElementById('reserve_test').checked ? 1 : 0;
+        // let isReserveTest = document.getElementById('reserve_test').checked ? 1 : 0;
 
         if (!interval || parseInt(interval) < MIN_RELOAD_INTERVAL_MILLISECONDS)
         {
@@ -75,13 +79,13 @@
 
         localStorage.setItem('NAVER_VACCINE_MACRO::vaccine', selected.id);
         localStorage.setItem('NAVER_VACCINE_MACRO::interval', interval);
-        localStorage.setItem('NAVER_VACCINE_MACRO::reserve_test', isReserveTest);
+        // localStorage.setItem('NAVER_VACCINE_MACRO::reserve_test', isReserveTest);
 
         // 크롬 스토리지에 입력 값 저장
         chrome.storage.local.set({
             selected_vaccine: getSelectedVaccineName(),
             interval: interval,
-            is_test: isReserveTest
+            // is_test: isReserveTest
         });
     }
 
@@ -98,166 +102,166 @@
         return "";
     }
 
-    const save = () => {
-        const botToken = document.getElementById('bot-token').value;
-        const chatId = document.getElementById('chat-id').value;
+    // const save = () => {
+    //     const botToken = document.getElementById('bot-token').value;
+    //     const chatId = document.getElementById('chat-id').value;
 
-        const msg = encodeURI('예약 알림이 연동되었습니다.');
-        const url = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${msg}`;
+    //     const msg = encodeURI('예약 알림이 연동되었습니다.');
+    //     const url = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${msg}`;
 
-        fetch(url).then(response => {
-            if (response.status === 200) {
-                chrome.storage.local.set({"botToken": botToken});
-                chrome.storage.local.set({"chatId": chatId});
-                localStorage.setItem('NAVER_VACCINE_MACRO::bot-token', botToken);
-                localStorage.setItem('NAVER_VACCINE_MACRO::chat-id', chatId);
-                setMessage(MESSAGE_CONNECTION_SUCCESS);
-            } else {
-                setMessage(MESSAGE_CONNECTION_FAIL);
-            }
-        }).catch(err => {
-            setMessage(MESSAGE_CONNECTION_FAIL);
-            console.error(err)
-        });
-    }
+    //     fetch(url).then(response => {
+    //         if (response.status === 200) {
+    //             chrome.storage.local.set({"botToken": botToken});
+    //             chrome.storage.local.set({"chatId": chatId});
+    //             localStorage.setItem('NAVER_VACCINE_MACRO::bot-token', botToken);
+    //             localStorage.setItem('NAVER_VACCINE_MACRO::chat-id', chatId);
+    //             setMessage(MESSAGE_CONNECTION_SUCCESS);
+    //         } else {
+    //             setMessage(MESSAGE_CONNECTION_FAIL);
+    //         }
+    //     }).catch(err => {
+    //         setMessage(MESSAGE_CONNECTION_FAIL);
+    //         console.error(err)
+    //     });
+    // }
 
-    const reset = () => {
-        document.getElementById('bot-token').value = '';
-        document.getElementById('chat-id').value = '';
-        localStorage.removeItem('NAVER_VACCINE_MACRO::bot-token');
-        localStorage.removeItem('NAVER_VACCINE_MACRO::chat-id');
-        chrome.storage.local.set("botToken", undefined);
-        chrome.storage.local.set("chatId", undefined);
-        setMessage(MESSAGE_RESET);
-    }
+    // const reset = () => {
+    //     document.getElementById('bot-token').value = '';
+    //     document.getElementById('chat-id').value = '';
+    //     localStorage.removeItem('NAVER_VACCINE_MACRO::bot-token');
+    //     localStorage.removeItem('NAVER_VACCINE_MACRO::chat-id');
+    //     chrome.storage.local.set("botToken", undefined);
+    //     chrome.storage.local.set("chatId", undefined);
+    //     setMessage(MESSAGE_RESET);
+    // }
 
-    const setMessage = message => {
-        document.getElementById('message').innerHTML = message;
-    }
+    // const setMessage = message => {
+    //     document.getElementById('message').innerHTML = message;
+    // }
 
-    const reload = () => {
-        chrome.runtime.reload();
-    }
+    // const reload = () => {
+    //     chrome.runtime.reload();
+    // }
 
-    const openAllLinks = () => {
-        const items = document.querySelectorAll('.hospital_item');
-        if (!items)
-            return;
-        for (let i = 0; i < items.length; ++i) {
-            let curr = items[i];
-            let anchor = curr.querySelector('.open_hospital_link');
-            if (anchor) {
-                chrome.tabs.create({
-                    url: anchor.href
-                });
-            }
-        }
-    }
+    // const openAllLinks = () => {
+    //     const items = document.querySelectorAll('.hospital_item');
+    //     if (!items)
+    //         return;
+    //     for (let i = 0; i < items.length; ++i) {
+    //         let curr = items[i];
+    //         let anchor = curr.querySelector('.open_hospital_link');
+    //         if (anchor) {
+    //             chrome.tabs.create({
+    //                 url: anchor.href
+    //             });
+    //         }
+    //     }
+    // }
 
-    const openSelectedLinks = () => {
-        const items = document.querySelectorAll('.hospital_item');
-        if (!items)
-            return;
-        let selected = new Array();
-        for (let i = 0; i < items.length; ++i) {
-            let curr = items[i];
-            let isChecked = curr.querySelector('input[name="open_hospital_link"]:checked');
-            if (isChecked) {
-                let anchor = curr.querySelector('.open_hospital_link');
-                if (anchor) selected.push(anchor);
-            }
-        }
+    // const openSelectedLinks = () => {
+    //     const items = document.querySelectorAll('.hospital_item');
+    //     if (!items)
+    //         return;
+    //     let selected = new Array();
+    //     for (let i = 0; i < items.length; ++i) {
+    //         let curr = items[i];
+    //         let isChecked = curr.querySelector('input[name="open_hospital_link"]:checked');
+    //         if (isChecked) {
+    //             let anchor = curr.querySelector('.open_hospital_link');
+    //             if (anchor) selected.push(anchor);
+    //         }
+    //     }
 
-        for (let i = 0; i < selected.length; ++i) {
-            chrome.tabs.create({
-                url: selected[i].href
-            });
+    //     for (let i = 0; i < selected.length; ++i) {
+    //         chrome.tabs.create({
+    //             url: selected[i].href
+    //         });
 
-        }
-    }
+    //     }
+    // }
 
-    const extract_list = () => {
-        const json = document.getElementById('graphql_result').value;
-        // console.log(json);
-        const object = JSON.parse(json);
-        if (!object)
-            return;
-        const list = object[0].data.rests.businesses.items;
-        // console.log(list);
-        if (!list)
-            return;
+    // const extract_list = () => {
+    //     const json = document.getElementById('graphql_result').value;
+    //     // console.log(json);
+    //     const object = JSON.parse(json);
+    //     if (!object)
+    //         return;
+    //     const list = object[0].data.rests.businesses.items;
+    //     // console.log(list);
+    //     if (!list)
+    //         return;
 
-        localStorage.setItem('NAVER_VACCINE_MACRO::hospital_list', json);
-        chrome.storage.local.set({hospital_list:JSON.stringify(list)});
+    //     localStorage.setItem('NAVER_VACCINE_MACRO::hospital_list', json);
+    //     chrome.storage.local.set({hospital_list:JSON.stringify(list)});
 
-        const desc = document.getElementById('hospital_results_desc');
-        const result = document.getElementById('hospital_results');
+    //     const desc = document.getElementById('hospital_results_desc');
+    //     const result = document.getElementById('hospital_results');
 
-        if (desc) {
-            desc.innerHTML = "";
-            desc.insertAdjacentHTML(
-                "beforeend",
-                `
-        <span>열기 클릭시 해당 병원 예약 신청 페이지로 이동</span>
-        <button type="button" class="button" id="open_selected_links">선택된 링크 열기</button>
-        <button type="button" class="button" id="open_all_links">모든 링크 열기</button>
-    `
-            );
-            const openSelectedBtn = document.getElementById('open_selected_links');
-            if (openSelectedBtn)
-                openSelectedBtn.addEventListener('click', openSelectedLinks);
-            const openAllBtn = document.getElementById('open_all_links');
-            if (openAllBtn)
-                openAllBtn.addEventListener('click', openAllLinks);
-        }
+    //     if (desc) {
+    //         desc.innerHTML = "";
+    //         desc.insertAdjacentHTML(
+    //             "beforeend",
+    //             `
+    //     <span>열기 클릭시 해당 병원 예약 신청 페이지로 이동</span>
+    //     <button type="button" class="button" id="open_selected_links">선택된 링크 열기</button>
+    //     <button type="button" class="button" id="open_all_links">모든 링크 열기</button>
+    // `
+    //         );
+    //         const openSelectedBtn = document.getElementById('open_selected_links');
+    //         if (openSelectedBtn)
+    //             openSelectedBtn.addEventListener('click', openSelectedLinks);
+    //         const openAllBtn = document.getElementById('open_all_links');
+    //         if (openAllBtn)
+    //             openAllBtn.addEventListener('click', openAllLinks);
+    //     }
 
-        result.innerHTML = "";
-        for (let i = 0; i < list.length; i++) {
-            const obj = list[i];
-            if (!obj)
-                continue;
-            const name = obj.name;
-            const distance = obj.distance;
-            let sid = '';
-            let orgCd = '';
-            let linkHTML = '';
-            if (obj.vaccineQuantity && obj.vaccineQuantity.vaccineOrganizationCode) {
-                orgCd = obj.vaccineQuantity.vaccineOrganizationCode;
-                sid = obj.id;
-                const url = RESERVE_URL + orgCd + RESERVER_URL_ADD_PARM + sid;
-                linkHTML = `<a href="${url}" target="_blank" class="open_hospital_link">열기</a>`;
-            } else {
-                linkHTML = '(orgCd 없음)';
-            }
+    //     result.innerHTML = "";
+    //     for (let i = 0; i < list.length; i++) {
+    //         const obj = list[i];
+    //         if (!obj)
+    //             continue;
+    //         const name = obj.name;
+    //         const distance = obj.distance;
+    //         let sid = '';
+    //         let orgCd = '';
+    //         let linkHTML = '';
+    //         if (obj.vaccineQuantity && obj.vaccineQuantity.vaccineOrganizationCode) {
+    //             orgCd = obj.vaccineQuantity.vaccineOrganizationCode;
+    //             sid = obj.id;
+    //             const url = RESERVE_URL + orgCd + RESERVER_URL_ADD_PARM + sid;
+    //             linkHTML = `<a href="${url}" target="_blank" class="open_hospital_link">열기</a>`;
+    //         } else {
+    //             linkHTML = '(orgCd 없음)';
+    //         }
 
-            result.insertAdjacentHTML(
-                "beforeend",
-                `
-        <li class="hospital_item">
-        <input type="checkbox" name="open_hospital_link"/>
-        <span>${name} ${distance} ${linkHTML}</span>
-        </li>
-    `
-            );
-        }
-    }
+    //         result.insertAdjacentHTML(
+    //             "beforeend",
+    //             `
+    //     <li class="hospital_item">
+    //     <input type="checkbox" name="open_hospital_link"/>
+    //     <span>${name} ${distance} ${linkHTML}</span>
+    //     </li>
+    // `
+    //         );
+    //     }
+    // }
 
-    const save_kakao_body = () => {
-        const json = document.getElementById('left_by_count_coords').value;
-        // console.log(json);
-        const object = JSON.parse(json);
-        if (!object)
-            return;
+    // const save_kakao_body = () => {
+    //     const json = document.getElementById('left_by_count_coords').value;
+    //     // console.log(json);
+    //     const object = JSON.parse(json);
+    //     if (!object)
+    //         return;
 
-        localStorage.setItem('NAVER_VACCINE_MACRO::kakao_body', json);
-        chrome.storage.local.set({kakao_body: JSON.stringify(object)});
-    }
+    //     localStorage.setItem('NAVER_VACCINE_MACRO::kakao_body', json);
+    //     chrome.storage.local.set({kakao_body: JSON.stringify(object)});
+    // }
 
     init();
 
-    document.getElementById('button-save').addEventListener('click', save);
-    document.getElementById('button-reset').addEventListener('click', reset);
-    document.getElementById('button-reload').addEventListener('click', reload);
-    document.getElementById('button-extract').addEventListener('click', extract_list);
-    document.getElementById('button-save-kakao-body').addEventListener('click', save_kakao_body);
+    // document.getElementById('button-save').addEventListener('click', save);
+    // document.getElementById('button-reset').addEventListener('click', reset);
+    // document.getElementById('button-reload').addEventListener('click', reload);
+    // document.getElementById('button-extract').addEventListener('click', extract_list);
+    // document.getElementById('button-save-kakao-body').addEventListener('click', save_kakao_body);
 })();
